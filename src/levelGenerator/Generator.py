44ead8b63bde60a0385aa3a05316e95e043268a1
Pierre <pier.engelstein@outlook.com>
2018-04-17 10:25:26 +0200

    Generator Frame started

--- src/levelGenerator/Generator.py
+++ src/levelGenerator/Generator.py
@@ -1,24 +1,27 @@
 #The level generator for the space invader
 from Tkinter import *
+import ttk
+import tkFont
+
+class Generator:
+    def __init__(self, master, width, height):
+        font = tkFont.Font(family = "Consolas", size = 10, weight = "normal") 
+        master.minsize(width=width, height=height)
+        master.maxsize(width=width, height=height)
+        ttk.Separator(master,orient=VERTICAL).place(x=(5*width)/8, y=0, height=height)
+        ttk.Separator(master,orient=HORIZONTAL).place(x=(5*width)/8, y=(5*height)/8, width=(3*width)/8)
+        Label(master, text = "Number of columns :").place(x = (5*width)/8 + 5, y = 10, width = font.measure("Number of columns :"), height = 20)
+        Label(master, text = "Number of lines :").place(x = (5*width)/8 + 5  , y = 80, width = font.measure("Number of lines :"), height = 20) 
+        inputCol = Entry(master)
+        inputCol.insert(0, "20")
+        inputCol.place(x = (5*width)/8 + 5, y = 30)
+        inputRow = Entry(master)
+        inputRow.insert(0, "20")
+        inputRow.place(x = (5*width)/8 + 5, y = 100)
 
 root = Tk()
+
 root.resizable(False, False)
 root.title("Space Invader Level Generator")
-
-# frame = Frame(root, width = "800", height = "600", bg = "white")
-# frame.pack()
-# label = Label(frame, text="Name", width="100", height="40", borderwidth=2, relief="groove")
-# label.grid(row = 0, column = 0, rowspan =2, sticky = W+E+N+S) 
-# 
-# label1 = Label(frame, text="Name", width="30", height="30", borderwidth=2, relief="groove")
-# label1.grid(row = 0, column = 1)
-# 
-# label2 = Label(frame, text="Name", width="30", height="10", borderwidth=2, relief="groove")
-# label2.grid(row = 1, column = 1)
-
-leftPanel = Frame(root, width = 800, height = 600).grid(row = 0, column = 0, rowspan =2) 
-rU = Frame(root, width = 50, height = 50).grid(row = 0, column = 1)
-rD = Frame(root, width = 50, height = 50).grid(row = 1, column = 1)
-
+gen = Generator(master = root, width  = 800, height = 600)
 root.mainloop()
-
