2f7b0f440b69a810c64807a98fa6c957fb78bcd0
Pierre <pier.engelstein@outlook.com>
2018-04-24 10:18:32 +0200

    Bounding box system addition
    - Added the bounding box system
    - Added detection of which entity is shooting the bullet (enemy / player)

--- src/EntityBullet.py
+++ src/EntityBullet.py
@@ -1,6 +1,6 @@
 class EntityBullet(object):
     
-    def __init__(self, x, y, width, height, speed, dmg, canvas):
+    def __init__(self, x, y, width, height, speed, dmg, ifa, canvas):
         self.posx = x
         self.posy = y
         self.width = width
@@ -8,18 +8,27 @@ class EntityBullet(object):
         self.speed = speed
         self.damages = dmg
         self.c = canvas
+        self.isFromAlien = ifa  #-1 => from player ; 1 => from alien
         self.form = canvas.create_oval(self.posx - self.width/2, self.posy - self.height/2, self.posx + self.width/2, self.posy + self.height/2, fill="white")
         self.life = 1
+        self.updateBBOX()
     
     def next(self):
-        self.dy = -1*self.speed
+        self.dy = self.isFromAlien*self.speed
         self.posy += self.dy
+        self.updateBBOX()
         return
     
     def render(self, canvas):
-        if(self.posy < 10): #OR if it touches an enemy
+        if(self.posy < 10 or self.posy > (self.c.winfo_height() - 50)): #OR if it touches an enemy
             canvas.delete(self.form)
             self.life = -1
         canvas.move(self.form, 0, self.dy)
         return
+    
+    def updateBBOX(self):
+        self.x0 = self.posx - self.width/2
+        self.y0 = self.posy - self.height/2
+        self.x1 = self.posx + self.width/2
+        self.y1 = self.posy + self.height/2
         
