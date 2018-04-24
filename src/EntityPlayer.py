7327284f3e20c505370fb4f49a1730da97913a46
Pierre <pier.engelstein@outlook.com>
2018-04-24 10:17:32 +0200

    Bounding Box system addition
    Added the bounding box system + boudning box update

--- src/EntityPlayer.py
+++ src/EntityPlayer.py
@@ -26,15 +26,27 @@ class EntityPlayer(object):
         image = image.resize((width, height))
         image.save("image.png", "png")
         self.pic = PhotoImage(file = "image.png")
+        print("Player image size : " + str(image.size))
         self.images = Canvas.create_image(self.posx, self.posy, image = self.pic)
+        self.updateBBOX()
+        self.bbox = self.c.create_rectangle(self.x0, self.y0, self.x1, self.y1, fill="white", stipple="gray50")
 
     def next(self):
         self.setLocation((self.c.winfo_pointerx() - self.c.winfo_rootx()), (self.c.winfo_pointery() - self.c.winfo_rooty()))
+        self.updateBBOX()
+        self.c.delete(self.bbox)
+        self.bbox = self.c.create_rectangle(self.x0, self.y0, self.x1, self.y1, fill="white", stipple="gray50")
         return
 
     def render(self, Canvas):
         Canvas.move(self.images, self.dx, self.dy)
         return
+    def updateBBOX(self):
+        self.x0 = self.posx - self.width/2
+        self.y0 = self.posy
+        self.x1 = self.posx + self.width/2
+        self.y1 = self.posy + self.height/2
+        
 #########################################################################        
     def setLocation(self, x, y):
         if(x < self.width/2): x = (self.width/2) + 1
@@ -44,7 +56,7 @@ class EntityPlayer(object):
         return
    
     def shootBullet(self, event):
-        bullet = EntityBullet(x = self.posx, y = self.posy - 20, width = 10, height = 10, speed = 10, dmg = 10, canvas = self.parent.c)
+        bullet = EntityBullet(x = self.posx, y = self.posy - 20, width = 10, height = 10, speed = 10, dmg = 10, ifa = -1, canvas = self.parent.c)
         self.parent.bulletList.append(bullet)
         return
     def addScore(self, score):
