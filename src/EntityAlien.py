e273180550e8795bf57c4a65d3f5ddfce693f1ff
Pierre <pier.engelstein@outlook.com>
2018-05-16 16:53:13 +0200

    XP management addition

--- src/EntityAlien.py
+++ src/EntityAlien.py
@@ -10,7 +10,7 @@ from EntityBullet import EntityBullet
 
 class EntityAlien(object):
 
-    def __init__(self, x, y, width, height, row, col, speed, canvas, parent, canShoot, UID, life = 30, EXP = 50):
+    def __init__(self, x, y, width, height, row, col, speed, canvas, parent, canShoot, UID, life = 30, SkinID = 1, EXP = 10):
         self.posx = x
         self.posy = y
         self.dx = 0
@@ -22,21 +22,21 @@ class EntityAlien(object):
         self.direction = 1
         self.speed = speed
         self.life = life
+        self.xp = EXP
         self.row = row
         self.col = col
         self.canShoot = canShoot
 #         self.form = canvas.create_rectangle(self.posx - self.width/2, self.posy - self.height/2, self.posx + self.width/2, self.posy + self.height/2, fill= 'white')
-        image = PIL.Image.open("resources/img/SpaceInvader_Enemy1.png")
+        image = PIL.Image.open("resources/img/SpaceInvader_Enemy" + str(SkinID) + ".png")
         image = image.resize((width, height))
-        image.save("resources/img/imgResized/SpaceInvader_Enemy1.png", "png")
-        self.pic = PhotoImage(file = "resources/img/imgResized/SpaceInvader_Enemy1.png")
+        image.save("resources/img/imgResized/SpaceInvader_Enemy" + str(SkinID) + ".png", "png")
+        self.pic = PhotoImage(file = "resources/img/imgResized/SpaceInvader_Enemy" + str(SkinID) + ".png")
         self.form = canvas.create_image(self.posx, self.posy, image = self.pic)
         self.parent.enemyList.append(self)
         self.updateBBOX()
         self.UID = UID
         
     def next(self):
-#         print ("posy (before) : ", self.posy)
         self.posx += self.parent.enemyGoesRight*self.speed
         if self.parent.flagUID >= self.UID:
             self.posx += self.parent.enemyGoesRight*self.speed
@@ -51,7 +51,6 @@ class EntityAlien(object):
             self.parent.currEnemy = -1
             
         self.updateBBOX()
-#         print ("posy (after) : ", self.posy)
         return
 
     def render(self, canvas):
