77e762105660af5014c42a595e12492630ebfc17
Pierre <pier.engelstein@outlook.com>
2018-05-16 16:54:41 +0200

    Changed from Message to create_text
    Allows us to have a transparent background

--- src/gui_button.py
+++ src/gui_button.py
@@ -6,27 +6,28 @@ except ImportError:
     from tkinter import *
 
 class gui_button(object):
-    def __init__(self, x, y, width, height, text, canvas, next, action, selected = False):
-        self.x = x
+    def __init__(self, x, y, width, height, text, canvas, next, action, selected = False, txtSize = 20):
+        self.x = x - width/2
         self.y = y
         self.width = width
         self.height = height
+        self.textSize = txtSize
         self.c = canvas
+        self.action = action
         self.selected = selected
         self.next = next
         self.prev = "null"
         self.txt = text
-        self.action = action
-        self.text = Message(self.c, text = text, justify=LEFT, width=200, background = "gray").place(x=self.x - self.width / 2, y=self.y - self.height/2, width=self.width, height=self.height)
+        self.text = self.c.create_text(self.x, self.y, fill = "white", anchor = "nw", text = self.txt, font=("Courier new", self.textSize))
         if(self.next != "null"):
             self.next.prev = self
     
     def draw(self):
         self.c.delete(self.text)
         if(self.selected == True):
-            self.text = Message(self.c, text = self.txt, justify=LEFT, width=200, background = "yellow").place(x=self.x - self.width / 2, y=self.y - self.height/2, width=self.width, height=self.height)
+            self.text = self.c.create_text(self.x, self.y, fill = "blue", anchor = "nw", text = self.txt, font=("Courier new", self.textSize))
         if(self.selected == False):
-            self.text = Message(self.c, text = self.txt, justify=LEFT, width=200, background = "gray").place(x=self.x - self.width / 2, y=self.y - self.height/2, width=self.width, height=self.height)
+            self.text = self.c.create_text(self.x, self.y, fill = "white", anchor = "nw", text = self.txt, font=("Courier new", self.textSize))
         return
     
     def action(self):
