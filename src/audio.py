0097580941f63a4d4ec2abac79e6027ca1ff3c53
Pierre <pier.engelstein@outlook.com>
2018-05-23 10:25:02 +0200

    First audio system implementation
    -Bug with using JACK + pulseaudio => too many error messages send

---
+++ src/audio.py
@@ -0,0 +1,30 @@
+import pyaudio
+import wave
+
+class audio(object):
+    
+    def __init__(self):
+        self.chunk_size=1024
+        self.initSoundList()
+        return
+    
+    def playSound(self, soundLocation):
+        print("Playing sound " + soundLocation)
+        wf = wave.open(soundLocation, 'rb')
+        p=pyaudio.PyAudio()
+        stream=p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                      channels=wf.getnchannels(),
+                      rate=wf.getframerate(),
+                      output=True)
+        data=wf.readframes(self.chunk_size)
+        while len(data) > 0:
+                stream.write(data)
+                data = wf.readframes(self.chunk_size)
+        
+        stream.stop_stream()
+        stream.close()
+        
+        p.terminate()
+    
+    def initSoundList(self):
+        self.clickSound="resources/sounds/click.wav"
