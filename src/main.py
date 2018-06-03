7997c26257747ccb2d49dcaca7f801fbcb16fb68
Pierre <pier.engelstein@outlook.com>
2018-06-03 16:13:57 +0200

    Cleaned main file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-#Flaaaaag Invader project
+#Space Invader project
 #Adrien Chotard, Pierre Engelstein
 
 import sys
-sys.dont_write_bytecode = True
+# sys.dont_write_bytecode = True
 
 try:
     # for Python2
@@ -20,17 +20,5 @@ root = Tk()
 root.resizable(False, False)
 
 c = Canvas(root, width=1200, height=700, bg='#565656')
-# gui_endMenuLost(width = 800, height = 600, canvas = c, root = root)
 
 mainMenu = gui_mainMenu(width = 1200, height = 700, canvas = c, root = root)
-
-# lvlParse = LevelParser("resources/levels/level2.spi")
-# lvlConf = lvlParse.parseFile()
-# if(lvlConf == 1):
-#     print ("Zbrah")
-# else:
-#     zbrah = 1
-#     if zbrah == 1 :
-#         display = Display(width=1200, height=700, tkinterRoot=root, lvlConf = lvlConf)
-#     else :
-#         display = Display(width=1200, height=700, tkinterRoot=root)
